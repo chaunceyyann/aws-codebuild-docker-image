2874bae35f1864a831f55e93cb147678bc7d1eb1
Chauncey Yan <chaunceyyann@gmail.com>
2025-05-13 01:32:38 -0400

    added missing vpc to root main

--- terraform/main.tf
+++ terraform/main.tf
@@ -13,4 +13,7 @@ module "codebuild" {
   project_name       = var.codebuild_project_name
   aws_region         = var.aws_region
   ecr_repository_arn = module.ecr.repository_arn
+  vpc_id             = var.vpc_id
+  private_subnet_ids = var.private_subnet_ids
+  ecr_repo_url      = module.ecr.repository_url
 }
