472dbf836cd1f94a334d50d2a1a121070550d855
Chauncey Yan <chaunceyyann@gmail.com>
2025-05-19 22:50:16 -0400

    add outputs project name to codebuild

--- terraform/modules/codebuild/outputs.tf
+++ terraform/modules/codebuild/outputs.tf
@@ -1,3 +1,8 @@
+output "project_name" {
+  description = "Name of the CodeBuild project"
+  value       = aws_codebuild_project.build.name
+}
+
 output "project_arn" {
   description = "ARN of the CodeBuild project"
   value       = aws_codebuild_project.build.arn
