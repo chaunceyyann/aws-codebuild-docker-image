fc576ae0e2a549e255bfcc54c76c459effc23cbb
Chauncey Yan <chaunceyyann@gmail.com>
2025-05-19 20:13:27 -0400

    Added vpc and subnet

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,5 +1,11 @@
 # aws-codebuild-docker-image/terraform/main.tf
 
+module "vpc" {
+  source = "./modules/vpc"
+
+  project_name = var.codebuild_project_name
+}
+
 module "ecr" {
   source = "./modules/ecr"
 
@@ -13,7 +19,7 @@ module "codebuild" {
   project_name       = var.codebuild_project_name
   aws_region         = var.aws_region
   ecr_repository_arn = module.ecr.repository_arn
-  vpc_id             = var.vpc_id
-  private_subnet_ids = var.private_subnet_ids
+  vpc_id             = module.vpc.vpc_id
+  private_subnet_ids = module.vpc.private_subnet_ids
   ecr_repo_url      = module.ecr.repository_url
 }
