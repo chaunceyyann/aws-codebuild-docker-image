25c6ef233b62b2a148e9d84ef2d4b4650cb24abe
Chauncey Yan <chaunceyyann@gmail.com>
2025-05-28 19:30:23 -0400

    vpc pubsubnet test

--- terraform/modules/vpc/main.tf
+++ terraform/modules/vpc/main.tf
@@ -19,6 +19,18 @@ resource "aws_subnet" "private" {
   }
 }
 
+resource "aws_subnet" "public" {
+  count             = 2
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = var.public_subnet_cidrs[count.index]
+  availability_zone = var.availability_zones[count.index]
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "${var.project_name}-public-${count.index + 1}"
+  }
+}
+
 resource "aws_internet_gateway" "main" {
   vpc_id = aws_vpc.main.id
 
@@ -35,15 +47,35 @@ resource "aws_route_table" "private" {
   }
 }
 
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name = "${var.project_name}-public-rt"
+  }
+}
+
 resource "aws_route_table_association" "private" {
   count          = 2
   subnet_id      = aws_subnet.private[count.index].id
   route_table_id = aws_route_table.private.id
 }
 
+resource "aws_route" "public_igw" {
+  route_table_id         = aws_route_table.public.id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = aws_internet_gateway.main.id
+}
+
+resource "aws_route_table_association" "public" {
+  count          = 2
+  subnet_id      = aws_subnet.public[count.index].id
+  route_table_id = aws_route_table.public.id
+}
+
 # Add NAT Gateway for private subnets to access internet
 resource "aws_eip" "nat" {
-  vpc = true
+  domain = "vpc"
 }
 
 resource "aws_nat_gateway" "nat" {
@@ -53,6 +85,8 @@ resource "aws_nat_gateway" "nat" {
   tags = {
     Name = "${var.project_name}-nat-gateway"
   }
+
+  depends_on = [aws_subnet.public]  # Ensure public subnets are created first
 }
 
 resource "aws_route" "private_nat" {
--- terraform/modules/vpc/variables.tf
+++ terraform/modules/vpc/variables.tf
@@ -15,8 +15,14 @@ variable "private_subnet_cidrs" {
   default     = ["10.0.1.0/24", "10.0.2.0/24"]
 }
 
+variable "public_subnet_cidrs" {
+  description = "List of CIDR blocks for public subnets"
+  type        = list(string)
+  default     = ["10.0.1.0/24", "10.0.2.0/24"]
+}
+
 variable "availability_zones" {
-  description = "Availability zones"
+  description = "List of availability zones"
   type        = list(string)
   default     = ["us-east-1a", "us-east-1b"]
 } 
