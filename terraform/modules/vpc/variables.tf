d759353dea6a22c936d2e01d22f8967801345e8f
Chauncey Yan <chaunceyyann@gmail.com>
2025-05-28 19:42:29 -0400

    update cidr to non existed

--- terraform/modules/vpc/variables.tf
+++ terraform/modules/vpc/variables.tf
@@ -12,13 +12,13 @@ variable "vpc_cidr" {
 variable "private_subnet_cidrs" {
   description = "CIDR blocks for private subnets"
   type        = list(string)
-  default     = ["10.0.3.0/24", "10.0.4.0/24"]
+  default     = ["10.0.5.0/24", "10.0.6.0/24"]
 }
 
 variable "public_subnet_cidrs" {
   description = "List of CIDR blocks for public subnets"
   type        = list(string)
-  default     = ["10.0.1.0/24", "10.0.2.0/24"]
+  default     = ["10.0.7.0/24", "10.0.8.0/24"]
 }
 
 variable "availability_zones" {
